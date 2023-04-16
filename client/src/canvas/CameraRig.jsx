f86f21a02d592f69761133b59f4e03b8ac45b4bb
Rohan Raj Gautam <rohanrajgautam@gmail.com>
2023-04-16 20:55:48 +0545

    feat: add lighting in threejs

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -7,6 +7,33 @@ import state from '../store'
 
 const CameraRig = ({ children }) => {
 	const group = useRef()
+	const snap = useSnapshot(state)
+
+	useFrame((state, delta) => {
+		const isBreakpoint = window.innerWidth <= 1260
+		const isMobile = window.innerWidth <= 600
+
+		// set the initial position of the model
+		let targetPostion = [-0.4, 0, 2]
+		if (snap.intro) {
+			if (isBreakpoint) targetPostion = [0, 0, 2]
+			if (isMobile) targetPostion = [0, 0.2, 2.5]
+		} else {
+			if (isMobile) targetPostion = [0, 0.5, 2.5]
+			else targetPostion = [0, 0, 2]
+		}
+
+		// set model camera postion
+		easing.damp3(state.camera.position, targetPostion, 0.25, delta)
+
+		// set the model rotation smoothly
+		easing.dampE(
+			group.current.rotation,
+			[state.pointer.y / 10, -state.pointer.x / 5, 0],
+			0.25,
+			delta
+		)
+	})
 
 	return <group ref={group}>{children}</group>
 }
